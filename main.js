fad3167e21178a7828edb8fe7f0d3d7450e6f794
Hùng Nguyễn <123824825+HaHngDuckk@users.noreply.github.com>
2024-05-03 22:39:43 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+/*function mail() {
+      window.open("Sorumaidcafe01@gmail.com", "_blank");
+} */
+function share() {
+      window.open("https://www.example.com", "_blank");
+    }
+
+
+
+function chat() {
+      window.open("https://www.messenger.com/t/150112781521301", "_blank");
+    }
+
+function maps() {
+      window.open("https://maps.app.goo.gl/Kz8w3is1U42FGvJaA", "_blank");
+    }
+    
+    
+    function facebook() {
+      window.open("https://www.facebook.com/sorumaidcafekawaii?mibextid=ZbWKwL", "_blank");
+    }
+    
+    function tiktok() {
+      window.open("https://www.tiktok.com/@sorumaidcafe", "_blank");
+    }
+    
+    function instagram() {
+      window.open("https://www.instagram.com/sorumaid_cafe/?funlid=Ygxcdrq7i0gJrA0I", "_blank");
+    }
